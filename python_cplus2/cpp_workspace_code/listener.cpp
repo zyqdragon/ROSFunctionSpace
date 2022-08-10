d81fac5f97794c894c2ea69aedc58e8cad9959cb
zyqdragon <zhangjdr@163.com>
2022-08-10 11:53:34 +0800

    Add files via upload

---
+++ python_cplus2/cpp_workspace_code/listener.cpp
@@ -0,0 +1,13 @@
+#include "ros/ros.h"
+#include "std_msgs/String.h"
+void chatterCallback(const std_msgs::String::ConstPtr& msg)
+//是一个回调函数，当接收到 chatter 话题的时候就会被调用。
+{ ROS_INFO("I heard: [%s]", msg->data.c_str());
+}
+int main(int argc, char **argv)
+{ ros::init(argc, argv, "listener");
+  ros::NodeHandle n;
+  ros::Subscriber sub = n.subscribe("chatter", 1000, chatterCallback);
+ros::spin();
+return 0; 
+}
