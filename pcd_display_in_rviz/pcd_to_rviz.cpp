e65699aa17287b12e40a8187d3d249b6a1f24fa2
zyqdragon <zhangjdr@163.com>
2022-09-21 23:16:33 -0700

    Add files via upload

---
+++ pcd_display_in_rviz/pcd_to_rviz.cpp
@@ -0,0 +1,29 @@
+//pcd_to_rviz.cpp
+#include<ros/ros.h>
+#include<pcl/point_cloud.h>
+#include<pcl_conversions/pcl_conversions.h>
+#include<sensor_msgs/PointCloud2.h>
+#include<pcl/io/pcd_io.h>
+//which contains the required definitions to load and store point clouds to PCD and other file formats.
+ 
+main (int argc, char **argv)
+{
+  ros::init (argc, argv, "UandBdetect");
+  ros::NodeHandle nh;
+  ros::Publisher pcl_pub = nh.advertise<sensor_msgs::PointCloud2> ("pcl_output1", 1);
+  pcl::PointCloud<pcl::PointXYZ> cloud;
+  sensor_msgs::PointCloud2 output;
+  pcl::io::loadPCDFile ("/home/zj/Desktop/basic_test/pcd_display_in_rviz/src/test.pcd", cloud);//更换为自己的pcd文件路径
+  //Convert the cloud to ROS message
+  pcl::toROSMsg(cloud, output);
+  output.header.frame_id = "odom1";//this has been done in order to be able to visualize our PointCloud2 message on the RViz visualizer，这里时fix frame的名字
+  ros::Rate loop_rate(1);
+  while (ros::ok())
+  {
+    pcl_pub.publish(output);
+    ros::spinOnce();
+    loop_rate.sleep();
+  }
+  return 0;
+}
+//https://blog.csdn.net/rabbit_flowers/article/details/115339767
